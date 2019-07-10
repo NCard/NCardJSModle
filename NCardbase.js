9632fdc0518e5939c0a7eee3229c1010b338198e
ncard <ncard0401@gmail.com>
2019-07-10 09:10:51 +0800

    change to js6

--- NCardbase.js
+++ NCardbase.js
@@ -1,6 +1,5 @@
-//格式化时间
 Date.prototype.format = function (f) {
-    var o = {
+    const o = {
         "M+": this.getMonth() + 1, // month
         "d+": this.getDate(), // day
         "H+": this.getHours(), // hour
@@ -10,11 +9,11 @@ Date.prototype.format = function (f) {
         "q+": Math.floor((this.getMonth() + 3) / 3), // quarter
         "S": this.getMilliseconds()
         // millisecond
-    }
+    };
     if (/(y+)/.test(f)) {
         f = f.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
     }
-    for (var k in o) {
+    for (const k in o) {
         if (new RegExp("(" + k + ")").test(f)) {
             f = f.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" +
                 o[k]).length));
